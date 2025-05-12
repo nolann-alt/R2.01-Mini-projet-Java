e36ea79e5696c6f531fba328ea190e55f8b6a329
nolann-alt <lescopnolann@gmail.com>
2025-05-12 15:12:07 +0200

    Update

--- src/Ensemble/MathsEnsemble.java
+++ src/Ensemble/MathsEnsemble.java
@@ -1,5 +1,6 @@
 package Ensemble;
 
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -7,7 +8,7 @@ import java.util.Set;
 public class MathsEnsemble<T> implements Ensemble<T> {
     private Set<T> ens;
 
-    public MathsEnsemble(T[] elements) {
+    public MathsEnsemble(Collection<T> elements) {
         ens = new LinkedHashSet<>();
 
         try {
@@ -37,27 +38,54 @@ public class MathsEnsemble<T> implements Ensemble<T> {
     }
 
 
-
     @Override
     public Ensemble<T> union(Ensemble<T> autre) {
-        LinkedHashSet<T> ret = new LinkedHashSet<>(ens);
-        for (T element : this.ens) {
-            ret.add(autre);
+        MathsEnsemble<T> ret = new MathsEnsemble<>(this.ens);
+
+        // On vérifie si l'autre ensemble est de type MathsEnsemble, si oui on l'ajoute à l'ensemble
+        if (autre instanceof MathsEnsemble<T> autreEnsemble) {
+            ret.ens.addAll(autreEnsemble.ens);
         }
-
-
         return ret;
     }
 
     @Override
     public Ensemble<T> intersection(Ensemble<T> autre) {
-        LinkedHashSet<T> intersection = new LinkedHashSet<>();
-        for (T elt : autre.ens) {
+        LinkedHashSet<T> result = new LinkedHashSet<>();
+        MathsEnsemble<T> autreEnsemble = (MathsEnsemble<T>) autre;
+        for (T elt : autreEnsemble.ens) {
             if (this.ens.contains(elt)) {
-                intersection.add(elt);
+                result.add(elt);
             }
         }
+        Ensemble<T> inter = new MathsEnsemble<>(result);
+        return inter;
     }
 
+    @Override
+    public Ensemble<T> difference(Ensemble<T> autre) {
+        LinkedHashSet<T> leHset = new LinkedHashSet<>();
+        MathsEnsemble<T> autreEnsemble = (MathsEnsemble<T>) autre;
+        for (T elt : autreEnsemble.ens) {
+            if (this.ens.contains(elt)) {
+                leHset.remove(elt);
+            }
+        }
+        Ensemble<T> ret = new MathsEnsemble<>(leHset);
+        return ret;
+    }
 
+    /*
+    public Ensemble<Couple<T, T>> produitCartesien(Ensemble<T> autre) {
+
+    }
+    */
+
+    public Set<T> elements() {
+        LinkedHashSet<T> ret = new LinkedHashSet<>();
+        for (T elt : this.ens) {
+            ret.add(elt);
+        }
+        return ret;
+    }
 }
