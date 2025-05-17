35885db2201c5784b1e753b49f3c3db1e65d9c5f
mgouelo <matthieugouelo@gmail.com>
2025-05-17 15:19:08 +0200

    Correction méthode transitivité + méthodes contient() et elements()

--- src/RelationBinaire/MathsRelation.java
+++ src/RelationBinaire/MathsRelation.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public class MathsRelation<T> {
 
-    private Set<Couple<T, T>> relabi;
+    private final Set<Couple<T, T>> relabi;
 
     public MathsRelation(Ensemble<Couple<T, T>> uneRelation) {
         this.relabi = new LinkedHashSet<>();
@@ -55,6 +55,11 @@ public class MathsRelation<T> {
         return symetrique;
     }
 
+    /**
+     * Checks if the binary relation is antisymmetric
+     * A relation R is antisymmetric if for all (a, b) and (b, a) in R, a == b
+     * @return true if antisymmetric, false otherwise
+     */
     public boolean estAntisymetrique() {
         boolean antisymetrique = true;
         T first;
@@ -69,18 +74,49 @@ public class MathsRelation<T> {
         return antisymetrique;
     }
 
+    /**
+     * Checks if the binary relation is transitive
+     * A relation R is transitive if for all (a, b) and (b, c) in R then (a, c) is also in R.
+     * @return true if transitive, false otherwise
+     */
     public boolean estTransitive() {
         boolean transitive = true;
-        T first;
-        T second;
-        T third;
-        for (Couple<T, T> cpl : this.relabi) {
-            first = cpl.getPremier();
-            second = cpl.getDeuxieme();
-            if (!this.relabi.contains(new MathsCouple<T, T>(first, third)) && !this.relabi.contains(new MathsCouple<T, T>(third, second))) {
-                transitive = false;
+        T first, second, third; // a, b, c
+        for (Couple<T, T> cpl : this.relabi) { // couple (a, b)
+            first = cpl.getPremier(); // a
+            second = cpl.getDeuxieme(); // b
+            for (Couple<T, T> cpl1 : this.relabi) {
+                if ( cpl1.getPremier().equals(second) ) { // couple (b, x)
+                    third = cpl1.getDeuxieme(); // c = x
+                    MathsCouple<T, T> coupleTransitif = new MathsCouple<>(first, third); // (a, x) --> chemin "raccourcis"
+                    if ( ! this.relabi.contains(coupleTransitif) ) {
+                        transitive = false;
+                    }
+                }
             }
         }
         return transitive;
+    }
+
+
+    /**
+     * Check if the relation contains a specific pair
+     * @param cpl the pair
+     * @return true if the relation contains the pair in parameters
+     */
+    public boolean contient(Couple<T, T> cpl) {
+        return this.relabi.contains(cpl);
+    }
+
+    /**
+     * Send to user the binary relation
+     * @return a copy of the binary relation
+     */
+    public Set<Couple<T, T>> elements() {
+        LinkedHashSet<Couple<T, T>> ret = new LinkedHashSet<>();
+        for (Couple<T, T> cpl : this.relabi) {
+            ret.add(cpl);
+        }
+        return ret;
     }
 }
